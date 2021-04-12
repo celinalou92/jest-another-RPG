331da37152f72d460714fce63601cc8d0f849fc4
Celina Louissaint <celinalouissaint@gmail.com>
2021-04-12 13:39:57 -0700

    methods for handling of potions and attack values added

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -69,3 +69,31 @@ test('checks if player is alive or not', () => {
   
     expect(player.health).toBe(0);
   });
+  // ------------Test Attack Method -----------// 
+  test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+  
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+  });
+
+    // ------------Test addPotion Method -----------//
+    test('adds a potion to the inventory', () => {
+        const player = new Player('Dave');
+        const oldCount = player.inventory.length;
+      
+        player.addPotion(new Potion());
+      
+        expect(player.inventory.length).toBeGreaterThan(oldCount);
+      });
+
+  // ------------Test us potion from inventory Method -----------//
+  test('uses a potion from inventory', () => {
+      const player = new Player('Dave');
+      player.inventory = [new Potion(), new Potion(), new Potion()];
+      const oldCount = player.inventory.length;
+
+      player.usePotion(1);
+      expect(player.inventory.length).toBeLessThan(oldCount);
+  });
